67f1a88fa006de125f56753b005989f5a321c654
Toby Wilkinson <toby.wilkinson339@gmail.com>
2021-01-03 18:25:17 +0000

    Trying new object

--- implicitCAD.py
+++ implicitCAD.py
@@ -1,22 +1,20 @@
-import time
 from Objects.DesignSpace import DesignSpace
-from Objects.Lattices.Gyroid import Gyroid
-from Objects.Shapes.Sphere import Sphere
+from Objects.Lattices.StrutLattice import ConvexHullLattice
+from Objects.Points.PointClouds import LHSPoints
 from Objects.Shapes.Cube import Cube
-from Objects.Shapes.Shape import Shape
-from Objects.Shapes.Cylinder import Cylinder
-from stl import mesh
 
 
 def main():
 
-    t = time.time()
+    ds = DesignSpace(res=100, xBounds=[-1.1, 1.1], yBounds=[-1.1, 1.1], zBounds=[-1.1, 1.1])
 
-    ds = DesignSpace(100)
+    cube = Cube(ds, round_r=1).previewModel()
 
-    print(time.time() - t)
+    pc = LHSPoints(n_points=2000, shape=cube)
 
+    lattice = ConvexHullLattice(ds, pc) + cube
+
+    lattice.previewModel()
 
 if __name__ == '__main__':
-
     main()
