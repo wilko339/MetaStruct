8393620ea6558ff11abd6288c23e45f8fcbdc33f
Toby Wilkinson <toby.wilkinson@addedscientific.com>
2021-05-21 13:19:24 +0100

    bracket lattice

--- implicitCAD.py
+++ implicitCAD.py
@@ -1,23 +1,27 @@
 from Objects.DesignSpace import DesignSpace
-from Objects.Shapes.Cube import Cube
 from Objects.Lattices.Gyroid import Gyroid
-from smt.surrogate_models import RMTB
-import numpy as np
-from Functions.ModifierArray import createModifierArray
+from Objects.Shapes.Cube import Cube
 
 from Objects.Shapes.ImportedMesh import ImportedMesh
 
-import igl
+from wildmeshing import Tetrahedralizer
 
 
 def main():
 
-    ds = DesignSpace(100, x_bounds=[-1, 180], y_bounds=[
+    ds = DesignSpace(200, x_bounds=[-1, 180], y_bounds=[
                      -1, 93], z_bounds=[-1, 112])
 
     bracket = ImportedMesh(ds, 'Engine_Bracket.stl')
 
-    bracket.previewModel()
+    bracket /= Gyroid(ds, lx=20, ly=20, lz=20)
+
+    bracket.findSurface()
+
+    bracket.decimate_mesh(0.5)
+
+
+    bracket.save_mesh('decimated')
 
 
 if __name__ == '__main__':
