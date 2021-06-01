6311ab2614ae158003e90843f014e3381caa7d5b
Toby Wilkinson <toby.wilkinson@addedscientific.com>
2021-06-01 09:11:08 +0100

    removed param check and updated var names

--- Objects/Shapes/Cylinder.py
+++ Objects/Shapes/Cylinder.py
@@ -9,33 +9,33 @@ class Cylinder(Shape):
     def __init__(self, design_space, x=0, y=0, z=0, r1=1, r2=1, l=1, ax='z'):
         super().__init__(design_space, x, y, z)
 
-        self.r1 = parameter_check(r1)
-        self.r2 = parameter_check(r2)
-        self.l = parameter_check(l)
+        self.r1 = r1
+        self.r2 = r2
+        self.l = l
         self.ax = ax
 
         if self.ax == 'z':
-            self.xLims = np.array(
+            self.x_limits = np.array(
                 [self.x - self.r1, self.x + self.r1])
-            self.yLims = np.array(
+            self.y_limits = np.array(
                 [self.y - self.r2, self.y + self.r2])
-            self.zLims = np.array(
+            self.z_limits = np.array(
                 [self.z - self.l, self.z + self.l])
 
         if self.ax == 'x':
-            self.xLims = np.array(
+            self.x_limits = np.array(
                 [self.x - self.l, self.x + self.l])
-            self.yLims = np.array(
+            self.y_limits = np.array(
                 [self.y - self.r1, self.y + self.r1])
-            self.zLims = np.array(
+            self.z_limits = np.array(
                 [self.z - self.r2, self.z + self.r2])
 
         if self.ax == 'y':
-            self.xLims = np.array(
+            self.x_limits = np.array(
                 [self.x - self.r1, self.x + self.r1])
-            self.yLims = np.array(
+            self.y_limits = np.array(
                 [self.y - self.l, self.y + self.l])
-            self.zLims = np.array(
+            self.z_limits = np.array(
                 [self.z - self.r2, self.z + self.r2])
 
     def __repr__(self):
@@ -45,8 +45,8 @@ class Cylinder(Shape):
     def __str__(self):
 
         string = super().__str__() + \
-                 f'\nAxis: {self.ax}' + \
-                 f'\nLength: {self.l}'
+            f'\nAxis: {self.ax}' + \
+            f'\nLength: {self.l}'
 
         if self.ax == 'z':
             return string + f'\nRadii(x, y): ({self.r1}, {self.r2})'
