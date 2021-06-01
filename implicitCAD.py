46a6dfb856bee28776dc6f20c8de5440fa9d575f
Toby Wilkinson <toby.wilkinson@addedscientific.com>
2021-06-01 08:56:59 +0100

    Minor

--- implicitCAD.py
+++ implicitCAD.py
@@ -1,27 +1,10 @@
-from Objects.DesignSpace import DesignSpace
-from Objects.Lattices.Gyroid import Gyroid
-from Objects.Shapes.Cube import Cube
+from Examples.LeosRings import leos_rings
 
-from Objects.Shapes.ImportedMesh import ImportedMesh
-
-from wildmeshing import Tetrahedralizer
+import igl
 
 
 def main():
-
-    ds = DesignSpace(200, x_bounds=[-1, 180], y_bounds=[
-                     -1, 93], z_bounds=[-1, 112])
-
-    bracket = ImportedMesh(ds, 'Engine_Bracket.stl')
-
-    bracket /= Gyroid(ds, lx=20, ly=20, lz=20)
-
-    bracket.findSurface()
-
-    bracket.decimate_mesh(0.5)
-
-
-    bracket.save_mesh('decimated')
+    leos_rings()
 
 
 if __name__ == '__main__':
