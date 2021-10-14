1e708e6a1d79bb853e101e320644009b6a01534c
Toby Wilkinson <65986341+wilko339@users.noreply.github.com>
2021-10-14 01:30:07 +0100

    Added example code and removed some old code

--- implicitCAD.py
+++ implicitCAD.py
@@ -1,14 +1,14 @@
 from Objects.Shapes.Cylinder import Cylinder
 from Objects.DesignSpace import DesignSpace
 from Objects.Lattices.Primitive import Primitive
-from Examples.LeosRings import leos_rings
-from P19045.lattice_slab import lattice_slabs
 
 
 def main():
-    # lattice_slabs()
-    leos_rings()
-
-
+    # Execute code here
+    ds = DesignSpace()
+    shape = Cylinder(ds)
+    shape /= Primitive(ds)
+    shape.previewModel()
+    
 if __name__ == '__main__':
     main()
