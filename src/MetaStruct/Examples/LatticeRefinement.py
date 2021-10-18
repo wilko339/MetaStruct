49e2eec483771bba37a61408274edf8278221ce2
Toby Wilkinson <toby.wilkinson339@gmail.com>
2021-10-18 14:03:59 +0100

    Minor updates

--- src/MetaStruct/Examples/LatticeRefinement.py
+++ src/MetaStruct/Examples/LatticeRefinement.py
@@ -2,18 +2,19 @@ import math
 
 import numpy as np
 
-from MetaStruct.Objects import Gyroid, Sphere
+from MetaStruct.Objects.Lattices.Gyroid import Gyroid
+from MetaStruct.Objects.Shapes.Sphere import Sphere
 from MetaStruct.Objects.designspace import DesignSpace
 from MetaStruct.Objects.Shapes.Cuboid import Cuboid
 
 
 def latticeRefinementExample():
 
-    ds = DesignSpace(resolution=300)
+    ds = DesignSpace(resolution=300, x_bounds=[-2, 2], y_bounds=[-2,2], z_bounds=[-0.5, 0.5])
 
     cuboid = Cuboid(ds, xd=2, yd=2, zd=0.5)
 
-    refinedLattice = Gyroid(ds, 1, ny=math.pi/3, nz=2, vf=0.4)
+    refinedLattice = Gyroid(ds, ny=math.pi/3, nz=2, vf=0.4)
 
     refinedLattice.convert_to_cylindrical()
 
@@ -28,4 +29,8 @@ def latticeRefinementExample():
 
     shape = cuboid / refinedLattice
 
-    shape.previewModel()
+    shape.preview_model()
+
+if __name__ == "__main__":
+
+    latticeRefinementExample()
