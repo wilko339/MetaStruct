0c8916d2debe958f65d968e25d57eaa0bf100fbf
toby <toby.wilkinson339@gmail.com>
2021-11-03 13:58:15 +0000

    No longer boolean

--- src/MetaStruct/testing/cube_test.py
+++ src/MetaStruct/testing/cube_test.py
@@ -8,11 +8,11 @@ from line_profiler_pycharm import profile
 @profile
 def main():
     ds = DesignSpace(resolution=500, create_grid=False)
-    cube = Cube(ds) + Sphere(ds, x=0.5)
+    cube = Cube(ds)
 
     cube.evaluate_grid()
 
-    cube.preview_model()
+    #cube.preview_model()
 
 
 # Original: 9074092
