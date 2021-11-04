946a1550531ede48b45a50c245e0db340b676370
toby <toby.wilkinson339@gmail.com>
2021-11-04 11:05:02 +0000

    Added conditional checking on ndims of vector components to avoid unnecessary numexpr calls, time saving

--- src/MetaStruct/Objects/Misc/Vector.py
+++ src/MetaStruct/Objects/Misc/Vector.py
@@ -1,4 +1,5 @@
 import numexpr as ne
+import numpy as np
 
 
 class Vector:
@@ -31,9 +32,22 @@ def subtract(a, b):
     by = b.y
     bz = b.z
 
-    arrx = ne.evaluate('ax-bx')
-    arry = ne.evaluate('ay-by')
-    arrz = ne.evaluate('az-bz')
+    if ax.ndim == 0 and bx.ndim == 0:
+        arrx = ax-bx
+    if ay.ndim == 0 and by.ndim == 0:
+        arry = ay-by
+    if az.ndim == 0 and bz.ndim == 0:
+        arrz = az-bz
+
+    else:
+
+        arrx = np.empty(ax.shape, dtype=np.float32)
+        arry = np.empty(ay.shape, dtype=np.float32)
+        arrz = np.empty(az.shape, dtype=np.float32)
+
+        ne.evaluate('a-b', local_dict={'a': ax, 'b': bx}, out=arrx, casting='same_kind')
+        ne.evaluate('a-b', local_dict={'a': ay, 'b': by}, out=arry, casting='same_kind')
+        ne.evaluate('a-b', local_dict={'a': az, 'b': bz}, out=arrz, casting='same_kind')
 
     return Vector([arrx, arry, arrz])
 
@@ -47,4 +61,10 @@ def mult(a, b):
     by = b.y
     bz = b.z
 
-    return ne.evaluate('(ax*bx)+(ay*by)+(az*bz)')
+    if ax.ndim == 0 and bx.ndim == 0:
+        return (ax*bx)+(ay*by)+(az*bz)
+
+    else:
+        out = np.empty_like(ax, dtype=np.float32)
+        ne.evaluate('(ax*bx)+(ay*by)+(az*bz)', out=out, casting='same_kind')
+        return out
