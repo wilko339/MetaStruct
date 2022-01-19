bba0600fb7840dd536b2854756e1b620f47df464
tobywilkinson <toby.wilkinson339@gmail.com>
2022-01-19 21:54:53 +0000

    Modified LatticeBracket.py to be a working example

--- src/MetaStruct/Examples/LatticeBracket.py
+++ src/MetaStruct/Examples/LatticeBracket.py
@@ -3,12 +3,30 @@ from MetaStruct.Objects.Shapes.ImportedMesh import ImportedMesh
 from MetaStruct.Objects.designspace import DesignSpace
 
 
-def lattice_bracket(filepath=None):
+"""
+This file shows how to import an existing triangular mesh and convert it into volumetric representation so it can work
+with the other primitives and lattices in the package.
+"""
 
-    ds = DesignSpace(100)
 
+# Change the argument here to point to the triangular mesh of choice.
+def lattice_bracket(filepath='Engine_Bracket.stl'):
+    # Initialise the design space as usual. Check the bounding box of your input mesh! This will be printed on import.
+    # Change the resolution of the design space to change the level of detail of the mesh import.
+    ds = DesignSpace(150, x_bounds=[-0.5, 179], y_bounds=[-0.5, 93], z_bounds=[-0.5, 112])
+
+    # Intialise the imported shape object
     shape = ImportedMesh(ds, filepath)
 
-    shape /= Gyroid(ds)
+    # Intersect the shape with a lattice or other shape of choice.
+    shape /= Gyroid(ds, lx=15, ly=15, lz=15)
 
-    shape.previewModel()
+    # Either preview or save the mesh.
+    shape.preview_model()
+
+
+def main():
+    lattice_bracket()
+
+if __name__ == '__main__':
+    main()
