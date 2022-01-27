8a4fd1c10a5d662472d94aea90ff8e46c2dde1ae
tobywilkinson <toby.wilkinson339@gmail.com>
2022-01-27 01:26:15 +0000

    Added the UnitCell and AxialCentric classes to allow lattice creation by unit cell instead of for each line. Added a test function for generating struts faster and some numba tests

--- src/MetaStruct/Objects/Lattices/StrutLattice.py
+++ src/MetaStruct/Objects/Lattices/StrutLattice.py
@@ -1,6 +1,7 @@
 import cProfile
 import io
 import pstats
+import time
 
 import numexpr as ne
 import numpy as np
@@ -8,24 +9,77 @@ import scipy
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 
-from MetaStruct.Objects.Shapes.Line import Line
+from MetaStruct.Objects.Shapes.Line import Line, SimpleLine
 from MetaStruct.Objects.Shapes.Shape import Shape
 
+from line_profiler_pycharm import profile
+from numba import njit, prange
 
-def profile(func):
-    def wrapper(*args, **kwargs):
-        pr = cProfile.Profile()
-        pr.enable()
-        retval = func(*args, **kwargs)
-        pr.disable()
-        s = io.StringIO()
-        sortby = 'cumulative'
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
-        return retval
 
-    return wrapper
+INDEXES = {
+    0: 'x',
+    1: 'y',
+    2: 'z'
+}
+
+
+def old_norm(vector):
+    return np.linalg.norm(vector, axis=0)
+
+
+@njit(parallel=True, fastmath=True)
+def pa_numba(vec, line):
+    out = np.empty_like(vec)
+    for i in prange(3):
+        out[i, :, :, :] = vec[i, :, :, :] - line[i]
+
+    return out
+
+@njit(fastmath=True)
+def pa_ba_h_numba(pa, ba_h):
+    out = np.empty([3, ba_h.shape[0], ba_h.shape[1], ba_h.shape[2]])
+    for i in prange(3):
+        for j in range(ba_h.shape[0]):
+            for k in range(ba_h.shape[0]):
+                for l in range(ba_h.shape[0]):
+                    out[i, j, k, l] = pa
+
+
+class UnitCell(Shape):
+    def __init__(self, design_space):
+        super().__init__(design_space)
+
+
+class AxialCentric(UnitCell):
+    def __init__(self, design_space, centre=None, r1=0.25, r2=0.25, r3=0.25, cell_size=1):
+        if centre is None:
+            centre = [0, 0, 0]
+        self.centre = centre
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
+        self.cell_size = cell_size
+
+        super().__init__(design_space)
+
+        self.x_limits = [self.centre[0]-self.cell_size/2, self.centre[0]+self.cell_size/2]
+        self.y_limits = [self.centre[1]-self.cell_size/2, self.centre[1]+self.cell_size/2]
+        self.z_limits = [self.centre[2]-self.cell_size/2, self.centre[2]+self.cell_size/2]
+
+        self.lines = []
+
+        for idx, r in enumerate([self.r1, self.r2, self.r3]):
+            if r > 0:
+                self.lines.append(SimpleLine(self.design_space, self.centre, self.cell_size, r, INDEXES[idx]))
+
+        self.unit_cell = self.lines[0]
+
+        if len(self.lines) > 1:
+            for i in range(1, len(self.lines)):
+                self.unit_cell += self.lines[i]
+
+    def evaluate_point(self, x, y, z):
+        return self.unit_cell.evaluate_point(x, y, z)
 
 
 class StrutLattice(Shape):
@@ -42,6 +96,7 @@ class StrutLattice(Shape):
             self.point_cloud = point_cloud
             self.points = self.point_cloud.points
 
+    @profile
     def generate_lattice(self):
 
         self.n_lines = len(self.lines)
@@ -89,6 +144,82 @@ class StrutLattice(Shape):
             b = self.blend
             yield ne.evaluate(
                 '-log(where((exp(-b*line_grid) + exp(-b*grid))>0.000, exp(-b*line_grid) + exp(-b*grid), 0.000))/b')
+
+    @profile
+    def generate_lattice_test(self):
+        """
+        This function is used to avoid instantiating many lines and doing boolean ops as its super slow.
+        Want to vectorise using numpy ideally...
+        """
+
+        data_type = self.design_space.DATA_TYPE
+
+        self.n_lines = len(self.lines)
+
+        print(f'Generating {self.n_lines} Struts...')
+
+        self.lines = np.array(self.lines, dtype=data_type)
+
+        vec = np.array([self.x_grid, self.y_grid, self.z_grid], dtype=data_type)
+
+        # Is there a way to avoid the ragged nested sequences error?
+        vec_bc = np.array([self.design_space.X[:, None, None], self.design_space.Y[None, :, None],
+                           self.design_space.Z[None, None, :]])
+
+        ba = self.lines[:, 1, :] - self.lines[:, 0, :]
+
+        baba = np.einsum('ij,ij->i', ba, ba, optimize='greedy')
+
+        grid = np.full_like(self.x_grid, np.inf, dtype=data_type)
+
+        for i, line in enumerate(self.lines[:, 0, :]):
+
+            # Wanna avoid this line (slow)
+            pa = vec - line[:, None, None, None]
+
+            # Need to find a way to use this
+            pa_bc = vec_bc - line
+
+            paba_bc = pa_bc * ba[i]
+
+            paba = np.dot(pa_bc, ba[i])
+
+            h = np.empty_like(paba, dtype=data_type)
+
+            np.clip(paba / baba[i], 0, 1, out=h)
+
+            #ba_h_ = ba[i][:, None, None, None] * h
+
+            ba_h = ne.evaluate('ba*h', local_dict={'ba':ba[i][:, None, None, None], 'h': h})
+
+            pa_ba_h = np.empty_like(vec, dtype=data_type)
+
+            print(pa_bc, ba_h)
+
+            raise
+
+            # Need to replace this implementation to use 'pa_bc' instead...
+            ne.evaluate('pa-ba_h', local_dict={'pa': pa, 'ba_h': ba_h}, out=pa_ba_h)
+
+            # Can this norm be made faster?
+            if len(self.r) == 1:
+                out = old_norm(pa_ba_h) - self.r
+            else:
+                out = old_norm(pa_ba_h) - self.r[i]
+
+            if self.blend == 0:
+
+                grid = ne.evaluate('where(grid<out, grid, out)', casting='same_kind')
+
+            else:
+
+                grid = ne.evaluate(
+                    '-log(where((exp(-b*out) + exp(-b*grid))>0.000, exp(-b*out) + exp(-b*grid), 0.000))/b',
+                    local_dict={'b': self.blend, 'grid': grid, 'out': out}, casting='same_kind')
+
+        self.evaluated_grid = grid
+
+        return
 
 
 class RandomLattice(StrutLattice):
@@ -287,9 +418,9 @@ class RegularStrutLattice(StrutLattice):
 
 class OptimisationLattice(StrutLattice):
 
-    def __init__(self, design_space, data, cell_size=10, scale_factor=0.5):
+    def __init__(self, design_space, data, cell_size=1, scale=10):
         super().__init__(design_space)
-        self.size = scale_factor
+        self.scale = scale
         self.cell_size = cell_size
         self.data = import_optimisation_data(data)
 
@@ -301,57 +432,69 @@ class OptimisationLattice(StrutLattice):
 
     def create_struts(self):
 
-        struts = []
-        strut_radii = []
+        uc_list = []
 
         for idx, row in self.data.iterrows():
-            x = row['x'] * self.cell_size
-            y = row['y'] * self.cell_size
-            z = row['z'] * self.cell_size
-            r1 = row['r1'] * self.size
-            r2 = row['r2'] * self.size
-            r3 = row['r3'] * self.size
-            r4 = row['r4'] * self.size
-            r5 = row['r5'] * self.size
-            r6 = row['r6'] * self.size
-            r7 = row['r7'] * self.size
+            x = row['x'] * self.scale
+            y = row['y'] * self.scale
+            z = row['z'] * self.scale
+            r1 = row['r1']
+            r2 = row['r2']
+            r3 = row['r3']
+            # r4 = row['r4'] * self.size
+            # r5 = row['r5'] * self.size
+            # r6 = row['r6'] * self.size
+            # r7 = row['r7'] * self.size
 
-            line1 = [[x - self.size / 2, y - self.size / 2, z + self.size / 2],
-                     [x + self.size / 2, y + self.size / 2, z - self.size / 2], r1]
+            uc_list.append([[x, y, z], [r1, r2, r3]])
 
-            line2 = [[x - self.size / 2, y + self.size / 2, z + self.size / 2],
-                     [x + self.size / 2, y - self.size / 2, z - self.size / 2], r2]
-
-            line3 = [[x - self.size / 2, y + self.size / 2, z - self.size / 2],
-                     [x + self.size / 2, y - self.size / 2, z + self.size / 2], r3]
-
-            line4 = [[x - self.size / 2, y - self.size / 2, z - self.size / 2],
-                     [x + self.size / 2, y + self.size / 2, z + self.size / 2], r4]
-
-            line5 = [[x - self.size / 2, y, z],
-                     [x + self.size / 2, y, z], r5]
-
-            line6 = [[x, y - self.size / 2, z],
-                     [x, y + self.size / 2, z], r6]
-
-            line7 = [[x, y, z - self.size / 2],
-                     [x, y, z + self.size / 2], r7]
-
-            for line in [line1, line2, line3, line4, line5, line6, line7]:
-                if line[2] > 0:
-                    struts.append([line[0], line[1]])
-                    strut_radii.append(line[2])
-
-        self.lines = struts
-        self.r = strut_radii
+        self.uc_list = uc_list
         self.generate_lattice()
+
+    def generate_lattice(self):
+
+        self.n_cells = len(self.uc_list)
+
+        try:
+            initial_uc = AxialCentric(self.design_space, self.uc_list[0][0], self.uc_list[0][1][0], self.uc_list[0][1][1],
+                                      self.uc_list[0][1][2], self.cell_size)
+
+        except IndexError:
+
+            print('No unit cells found.')
+
+            raise
+
+        print(f'Generating Lattice with {self.n_cells} unit cells...')
+
+        initial_uc.evaluate_grid(verbose=False)
+
+        self.evaluated_grid = initial_uc.evaluated_grid
+
+        for i in range(1, len(self.uc_list)):
+            self.evaluated_grid = next(self.new_grid(self.uc_list[i], i))
+
+    def new_grid(self, uc, idx):
+
+        uc = AxialCentric(self.design_space, self.uc_list[idx][0], self.uc_list[idx][1][0], self.uc_list[idx][1][1],
+                                  self.uc_list[idx][1][2], self.cell_size)
+
+        uc.evaluate_grid(verbose=False)
+
+        uc_grid = uc.evaluated_grid
+
+        grid = self.evaluated_grid
+
+        yield ne.evaluate('where(grid<uc_grid, grid, uc_grid)')
 
 
 def import_optimisation_data(path):
-    data = pd.read_csv(path, nrows=100, header=None)
+    data = pd.read_csv(path, nrows=2000, header=None)
     data.columns = ['x', 'y', 'z', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7']
     data = data.drop(data[(data.r1 == 0) & (data.r2 == 0) & (data.r3 == 0) & (data.r4 == 0) & (data.r5 == 0) &
                           (data.r6 == 0) & (data.r7 == 0)].index)
+
+    data = data.drop(data[(data.r1 == 0) & (data.r2 == 0) & (data.r3 == 0)].index)
 
     return data
 
