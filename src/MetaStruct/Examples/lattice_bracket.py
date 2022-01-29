a767c52ea61ff6b2a3eb5fc64a4a9ec447046be3
tobywilkinson <toby.wilkinson339@gmail.com>
2022-01-29 23:14:56 +0000

    Added latticing to bracket example

--- src/MetaStruct/Examples/lattice_bracket.py
+++ src/MetaStruct/Examples/lattice_bracket.py
@@ -1,6 +1,8 @@
 from MetaStruct.Objects.Lattices.Gyroid import Gyroid
 from MetaStruct.Objects.Shapes.ImportedMesh import ImportedMesh
 from MetaStruct.Objects.designspace import DesignSpace
+from MetaStruct.Objects.Lattices.StrutLattice import RepeatingLattice, OctetTruss
+from MetaStruct.Objects.Shapes.Cube import Cube
 
 
 """
@@ -13,16 +15,26 @@ with the other primitives and lattices in the package.
 def lattice_bracket(filepath='Engine_Bracket.stl'):
     # Initialise the design space as usual. Check the bounding box of your input mesh! This will be printed on import.
     # Change the resolution of the design space to change the level of detail of the mesh import.
-    ds = DesignSpace(150, x_bounds=[-0.5, 179], y_bounds=[-0.5, 93], z_bounds=[-0.5, 112])
+    ds = DesignSpace(100, x_bounds=[-0.5, 179], y_bounds=[-0.5, 93], z_bounds=[-0.5, 112])
 
     # Intialise the imported shape object
     shape = ImportedMesh(ds, filepath)
 
     # Intersect the shape with a lattice or other shape of choice.
-    shape /= Gyroid(ds, lx=15, ly=15, lz=15)
+    unit_cell = OctetTruss(ds, [0, 0, 0], cell_size=20, r=2)
+
+    uc = RepeatingLattice(ds, unit_cell=unit_cell, period=20, r=0)
+
+    lattice_region = shape / uc
+
+    lattice_region.evaluate_grid()
+
+    shape.shell(4)
+
+    final = shape + lattice_region
 
     # Either preview or save the mesh.
-    shape.preview_model()
+    final.preview_model(clip='x', clip_value=90)
 
 
 def main():
