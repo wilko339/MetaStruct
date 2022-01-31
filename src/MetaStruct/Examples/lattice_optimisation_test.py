5a3f0f4997fbed9f175f1339a596662263cdd2f2
tobywilkinson <toby.wilkinson339@gmail.com>
2022-01-31 21:07:46 +0000

    Testing

--- src/MetaStruct/Examples/lattice_optimisation_test.py
+++ src/MetaStruct/Examples/lattice_optimisation_test.py
@@ -1,12 +1,25 @@
 from MetaStruct.Objects.designspace import DesignSpace
-from MetaStruct.Objects.Lattices.StrutLattice import OptimisationLattice
+from MetaStruct.Objects.Shapes.Sphere import Sphere
+from MetaStruct.Objects.Shapes.Cube import Cube
+from MetaStruct.Objects.Lattices.StrutLattice import RepeatingLattice, AxialCentric, BCCAxial, OctetTruss
+from MetaStruct.Functions.ModifierArray import create_modifier_array
+from MetaStruct.Objects.Shapes.Line import Line
+import numpy as np
+from line_profiler_pycharm import profile
 
-
+@profile
 def main():
-    ds = DesignSpace(resolution=150, x_bounds=[-1, 11], y_bounds=[-1, 11], z_bounds=[-1, 11])
-    lattice = OptimisationLattice(ds, 'AM_Cube_LB007_BD_X_B_100.csv')
+    ds = DesignSpace(resolution=250, x_bounds=[-5, 5], y_bounds=[-5, 5], z_bounds=[-5, 5])
 
-    lattice.preview_model()
+    cell = OctetTruss(ds, cell_size=2, r=0.1)
+
+    uc = RepeatingLattice(ds, unit_cell=cell, period=2, r=0) / Cube(ds, dim=3)
+
+    uc.vector_rotation()
+
+    uc.preview_model()
+
+    return
 
 if __name__ == '__main__':
     main()
