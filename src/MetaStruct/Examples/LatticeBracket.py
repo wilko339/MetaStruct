f6cc9a184d36b8f6b169084984fe5807b20a02bc
tobywilkinson <toby.wilkinson339@gmail.com>
2022-02-01 00:58:10 +0000

    Updated

--- src/MetaStruct/Examples/LatticeBracket.py
+++ src/MetaStruct/Examples/LatticeBracket.py
@@ -5,10 +5,16 @@ from MetaStruct.Objects.designspace import DesignSpace
 
 def lattice_bracket(filepath=None):
 
-    ds = DesignSpace(100)
+    ds = DesignSpace(100, x_bounds=[-1, 180], y_bounds=[-1, 93], z_bounds=[-1, 112])
 
     shape = ImportedMesh(ds, filepath)
 
+    shape.preview_model()
+
     shape /= Gyroid(ds)
 
-    shape.previewModel()
+    shape.preview_model()
+
+
+if __name__ ==  '__main__':
+    lattice_bracket('/Users/tobywilkinson/PycharmProjects/MetaStruct/src/MetaStruct/Engine_Bracket.STL')
