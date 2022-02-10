175c014e443632c4f0f5e98551875470a20494bc
Toby Wilkinson <65986341+wilko339@users.noreply.github.com>
2022-02-10 12:02:28 +0000

    Remove previous profiling function, minor changes

--- src/MetaStruct/testing/voronoi_test.py
+++ src/MetaStruct/testing/voronoi_test.py
@@ -2,37 +2,21 @@ from MetaStruct.Objects.designspace import DesignSpace
 from MetaStruct.Objects.Shapes.Cube import Cube
 from MetaStruct.Objects.Shapes.Sphere import Sphere
 from MetaStruct.Objects.Shapes.Shape import Shape
-from MetaStruct.Objects.Points.PointClouds import *
+from MetaStruct.Objects.Points.PointClouds import RandomPoints, LHSPoints
 from MetaStruct.Objects.Lattices.StrutLattice import *
 from MetaStruct.Objects.Booleans.Boolean import *
 
-import cProfile
-import io
-import pstats
-
-def profile(func):
-    def wrapper(*args, **kwargs):
-        pr = cProfile.Profile()
-        pr.enable()
-        retval = func(*args, **kwargs)
-        pr.disable()
-        s = io.StringIO()
-        ps = pstats.Stats(pr, stream=s).sort_stats('tottime')
-        ps.print_stats()
-        print(s.getvalue())
-        return retval
-    return wrapper
-
-@profile
+s
 def voro_test():
-    ds = DesignSpace(100, create_grid=True)
+    ds = DesignSpace(300)
     region = Sphere(ds)
 
-    points = RandomPoints(40, region, seed=1)
+    points = LHSPoints(ds, 50, shape=region)
 
-    lat = VoronoiLattice(ds, points, r=0.03)
+    lat = DelaunayLattice(ds, point_cloud=points, r=0.03) / region
+    lat += VoronoiLattice(ds, point_cloud=points, r=0.03) / region
 
-    #lat.preview_model()
+    lat.preview_model()
 
 if __name__ == "__main__":
     voro_test()
