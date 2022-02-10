2d3e09de4aa186b452b4644e8717c75fdce54bf0
Toby Wilkinson <65986341+wilko339@users.noreply.github.com>
2022-02-10 12:03:30 +0000

    Removed vector rotate (not implemented yet)

--- src/MetaStruct/Examples/lattice_optimisation_test.py
+++ src/MetaStruct/Examples/lattice_optimisation_test.py
@@ -3,21 +3,19 @@ from MetaStruct.Objects.Shapes.Sphere import Sphere
 from MetaStruct.Objects.Shapes.Cube import Cube
 from MetaStruct.Objects.Lattices.StrutLattice import RepeatingLattice, AxialCentric, BCCAxial, OctetTruss
 from MetaStruct.Functions.ModifierArray import create_modifier_array
-from MetaStruct.Objects.Shapes.Line import Line
+from MetaStruct.Objects.Shapes.Line import Line, LineZ
 import numpy as np
 from line_profiler_pycharm import profile
 
 @profile
 def main():
-    ds = DesignSpace(resolution=250, x_bounds=[-5, 5], y_bounds=[-5, 5], z_bounds=[-5, 5])
+    ds = DesignSpace(resolution=300, x_bounds=[-5, 5], y_bounds=[-5, 5], z_bounds=[-5, 5])
 
-    cell = OctetTruss(ds, cell_size=2, r=0.1)
+    cell = OctetTruss(ds, cell_size=2, r=0.15)
 
-    uc = RepeatingLattice(ds, unit_cell=cell, period=2, r=0) / Cube(ds, dim=3)
+    uc = RepeatingLattice(ds, unit_cell=cell, period=2, r=0) / Cube(ds, dim=4)
 
-    uc.vector_rotation()
-
-    uc.preview_model()
+    uc.evaluate_grid()
 
     return
 
