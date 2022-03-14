8841f711c791fbe02b1b986ae13714f465c99bd3
Toby Wilkinson <65986341+wilko339@users.noreply.github.com>
2022-03-14 16:51:37 +0100

    Added some model previewing goodness

--- src/MetaStruct/testing/support_generation.py
+++ src/MetaStruct/testing/support_generation.py
@@ -6,12 +6,20 @@ from MetaStruct.Objects.Shapes.SupportVolume import SupportVolume
 
 
 def main():
-    ds = DesignSpace(resolution=400, x_bounds=[-1.1, 1.1], y_bounds=[-1.1, 1.1], z_bounds=[-1.1, 2.1])
-    shape = Sphere(ds, z=0.5) / Gyroid(ds, vf=0.5)
+    ds = DesignSpace(resolution=300, x_bounds=[-1.1, 1.1], y_bounds=[-1.1, 1.1], z_bounds=[-1.1, 2.1])
+    shape = Sphere(ds, z=0.5) / Gyroid(ds, vf=0.3)
+
+    shape.preview_model()
+
+    shape.preview_projection()
 
     support = SupportVolume(shape)
 
+    support.preview_model()
+
     support /= Gyroid(ds, lx=0.3, ly=0.3, lz=0.3, vf=0.2)
+
+    support.preview_model()
 
     support += shape
 
